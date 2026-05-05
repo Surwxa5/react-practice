61c5a95d73eb310996d573d4e802319ecec078bc
Surwxa5 <nepalisurakshya767@gmail.com>
2026-05-05 17:23:24 +0545

    product cart style

--- PracticeReact/src/App.jsx
+++ PracticeReact/src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from 'react'
+import ProductCard from './component/ProductCard'
 function App( ) {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <div>
-      
+    <div style={{maxWidth:"800px", margin:"0auto",padding:"20px",}}>
+      <h1>Product List</h1>
+      <div style={{display:"grid",gridTemplateColumns:"repeat(auto-fil,minmax(200px,1fr))",gap:"16px"}}>
+      <ProductCard name="Laptop" price={15000} inStock={true}/>
+      <ProductCard name="Laptop" price={15000} inStock={false}/>
+      <ProductCard name="Mouse" price={500} inStock={true}/>
+      <ProductCard name="Monitor" price={3000} inStock={false}/>
+      <ProductCard name="Keyboard" price={800} inStock={true}/>
+      </div>
     </div>
     </>
   )
--- PracticeReact/src/component/ProductCard.jsx
+++ PracticeReact/src/component/ProductCard.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 
 const ProductCard = ({name,price,inStock}) => {
   return (
-    <div>
+    <div style={{border:"1px solid black",borderRadius:"8px",padding:"16px",marginLeft:"8px"}}>
       <h1>{name}</h1>
       <p>{price}</p>
-      <p>{inStock?<span className='green'>In Stock</span> : <span className='red'>Out of Stock</span>}</p>
+    <p>{inStock ? <span style={{color: "green",fontWeight: "bold"}}>In Stock</span>
+    : <span style={{ color:"red", fontWeight: "bold"}}>Out of Stock</span>
+} 
+    </p>
     </div>
   )
 }
