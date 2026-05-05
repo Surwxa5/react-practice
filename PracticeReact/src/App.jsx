a9f46dd65ee4414ee07169c744fb286f67e35532
Surwxa5 <nepalisurakshya767@gmail.com>
2026-05-05 18:02:32 +0545

    usestate

--- PracticeReact/src/App.jsx
+++ PracticeReact/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import ProductCard from './component/ProductCard'
+import Likebutton from './component/Likebutton'
 function App( ) {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <div style={{maxWidth:"800px", margin:"0auto",padding:"20px",}}>
+    <div style={{maxWidth:"800px", margin:"auto",padding:"20px",}}>
       <h1>Product List</h1>
-      <div style={{display:"grid",gridTemplateColumns:"repeat(auto-fil,minmax(200px,1fr))",gap:"16px"}}>
+      <div style={{display:"grid",gridTemplateColumns:"repeat(auto-fill,minmax(200px,1fr))",gap:"16px"}}>
       <ProductCard name="Laptop" price={15000} inStock={true}/>
       <ProductCard name="Laptop" price={15000} inStock={false}/>
       <ProductCard name="Mouse" price={500} inStock={true}/>
@@ -15,6 +16,8 @@ function App( ) {
       <ProductCard name="Keyboard" price={800} inStock={true}/>
       </div>
     </div>
+    {/* use state practice */}
+    <Likebutton/>
     </>
   )
 }
---
+++ PracticeReact/src/component/Likebutton.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+const Likebutton = () => {
+  const [count,setCount] = useState(0)
+  return (
+    <div>
+      <p>Likes :{count}</p>
+      <button onClick={()=> setCount(count+1)} style={{backgroundColor:count>10 ? "red":"white"}}>Heart</button>
+      <button onClick={()=>setCount(0)
+      }>Reset</button>
+    </div>
+  )
+}
+
+export default Likebutton
