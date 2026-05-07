63d1dd6b21cff905300f8ba620907c35d73c5292
Surwxa5 <nepalisurakshya767@gmail.com>
2026-05-07 15:34:13 +0545

    delete todo

--- PracticeReact/src/component/Todolist.jsx
+++ PracticeReact/src/component/Todolist.jsx
@@ -5,6 +5,17 @@ const Todolist = () => {
   const [todos, setTodos] = useState([])
   // whats typed in the box
   const [input, setInput] = useState("")
+
+  const addTodo = () =>{
+    if (input.trim()=== "")
+      return 
+    setTodos([...todos,{id:Date.now(),text:input}])
+    setInput= ""
+
+    const DeleteTodo = (id) =>{
+      setTodos(todos.filter(todo =>todo.id !== id))
+    }
+  }
   return (
     <div>
       <input type="text" value={input} onChange={(e) => setInput(e.target.value)} placeholder='Add a todo....' />
