f11f7b35d5e546f9a565136f178722c39b963c41
Surwxa5 <nepalisurakshya767@gmail.com>
2026-05-08 15:33:23 +0545

    todo list

--- PracticeReact/src/component/Todolist.jsx
+++ PracticeReact/src/component/Todolist.jsx
@@ -17,11 +17,19 @@ const Todolist = () => {
     }
   }
   return (
-    <div>
-      <input type="text" value={input} onChange={(e) => setInput(e.target.value)} placeholder='Add a todo....' />
-      <button onClick={()=>{setTodos([...todos,{id:Date.now(),text:input}])
-      setInput("")  //clear the box after adding
-    }}>Add</button>
+    <div style={{maxWidth:"500px", margin:"40px auto",padding:"20px"}}>
+      <h1>Todo List</h1>
+      <p>{todo.length}todos remaining</p>
+    {/* for input section and button */}
+      <div style={{display:"flex", gap:"8px",marginBottom:"20px"}}>
+      <input type="text" value={input} onChange={(e) => setInput(e.target.value)}
+      onKeyDown={(e)=>e.key === "Enter" && addTodo} placeholder='Add a todo....' />
+      <button 
+       onClick={addTodo}
+    >Add</button>
+    </div>
+    {todos.length === 0}
+
     </div>
   )
 }
